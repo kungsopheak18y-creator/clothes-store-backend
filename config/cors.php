e5c7785274a7494561ee9637d517346039b85fd5
Sopheak Kung <kungsopheak18y@gmail.com>
2026-05-16 17:41:49 +0700

    fix cors

--- config/cors.php
+++ config/cors.php
@@ -8,10 +8,11 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'http://localhost:3000',
-        'https://clothes-store-wb.vercel.app',
+        'https://clothes-store-frontend-beta.vercel.app',
+        'https://clothes-store-frontend-qco2xlyrf-kungsopheak18y-1095s-projects.vercel.app',
     ],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['https://.*\.vercel\.app'],
 
     'allowed_headers' => ['*'],
 
