9aff7aab8b46c8053313427e5b61f6434ba2b9f8
Sopheak Kung <kungsopheak18y@gmail.com>
2026-05-16 18:02:45 +0700

    fix

--- public/index.php
+++ public/index.php
@@ -1,5 +1,14 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, Accept');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
