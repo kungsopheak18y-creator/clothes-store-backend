69aefab930263eae1f5dcc955bee14b46ebf8857
Sopheak Kung <kungsopheak18y@gmail.com>
2026-05-17 01:46:41 +0700

    fix cors credentials

--- config/cors.php
+++ config/cors.php
@@ -3,14 +3,10 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [
-        'https://clothes-store-frontend-sage.vercel.app',
-        'https://clothes-store-frontend-gt8qhpm20-kungsopheak18y-1095s-projects.vercel.app',
-        'http://localhost:5173',
-    ],
-    'allowed_origins_patterns' => ['#^https://.*\.vercel\.app$#'],
+    'allowed_origins' => ['*'],
+    'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 ];
